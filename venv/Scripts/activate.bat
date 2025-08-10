183a55f34c75d2984d5d491b61c22af481193b9c
tpre0013 <tpre0013@student.monash.edu>
2025-08-10 19:59:25 +1000

    [Fixed] - version install requirement.txt

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\65445\TA45-develop-map\venv
+set VIRTUAL_ENV=C:\Users\thari\Documents\FIT5120\TA45\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
