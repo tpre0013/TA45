b9461e476d360312e1d399aa369a98d64d2cddb9
tpre0013 <tpre0013@student.monash.edu>
2025-08-11 12:41:51 +1000

    [Fixed] - conflig

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
-=======
 from django.contrib import admin
 from django.urls import path, include
 from api.views import home_page, traffic_page 
@@ -30,5 +6,4 @@ urlpatterns = [
     path('traffic/', traffic_page, name='traffic'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  
->>>>>>> origin/aws/hosting
 ]
